b8fac1b1bb13ead4b243cc39302854b95353cfeb
Nahuel <nahuel.ng240@gmail.com>
2024-04-23 11:38:56 -0300

    Cree los metodos para la clase Cuadrado

--- src/Model/Cuadrado.java
+++ src/Model/Cuadrado.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Model;
 
-/**
- *
- * @author Nahu
- */
-public class Cuadrado {
+package Model;
+import Interface.ICalcularFig;
+
+public class Cuadrado implements ICalcularFig {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
     
+    @Override
+    public double calcSup(){
+        return lado * lado; 
+    }
+    
+    @Override
+    public double calcPer() {
+       return 4 * lado;
+    }
 }
