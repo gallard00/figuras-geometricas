26f1e2132b45b1ab2c7d2ae0eae2167f27331407
Nahuel <nahuel.ng240@gmail.com>
2024-04-23 11:31:39 -0300

    Cree los metodos para Circulo

--- src/Model/Circulo.java
+++ src/Model/Circulo.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Model;
 
-/**
- *
- * @author Nahu
- */
-public class Circulo {
+package Model;
+import Interface.ICalcularFig;
+
+public class Circulo implements ICalcularFig {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
     
+    @Override
+    public double calcSup() {
+        return (double) (Math.PI * radio * radio);
+    }
+
+    @Override
+    public double calcPer() {
+        return (double) (2 * Math.PI * radio);
+    }
 }
