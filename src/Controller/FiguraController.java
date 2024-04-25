ddd41700c90f4e749d06becc4047f69c3baceb54
Nahuel <nahuel.ng240@gmail.com>
2024-04-25 13:01:30 -0300

    Cree todos los metodos para la controladora de las figuras geometricas

--- src/Controller/FiguraController.java
+++ src/Controller/FiguraController.java
@@ -4,20 +4,70 @@ import Model.Circulo;
 import Model.Cuadrado;
 import Model.Triangulo;
 import Interface.ICalcularFig;
+import View.ViewFiguras;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class FiguraController {
     private static FiguraController Instance;
-    private List<ICalcularFig> ListaFig = new ArrayList<ICalcularFig>();
+    private ViewFiguras vista;
+    private ICalcularFig figura;
     
-    public static FiguraController GetInstance() throws SQLException {
+    public static FiguraController GetInstance(ViewFiguras vista) throws SQLException {
         if (Instance == null) {
-            Instance = new FiguraController();
+            Instance = new FiguraController(vista);
         }
         return Instance;
     }
+
+    public FiguraController(ViewFiguras vista) {
+        this.vista = vista;
+        this.vista.setFiguraController(this);
+    }
     
+    public void calcularFigura (String tipoFigura, double... parametros){
+        switch (tipoFigura) {
+            case "Cuadrado":
+                if (parametros.length == 1) {
+                    figura = new Cuadrado (parametros[0]);
+                } else {
+                   JOptionPane.showMessageDialog(null, "Número de parámetros incorrecto para un cuadrado.");
+                    return; 
+                }
+                break;
+                case "Triangulo":
+                if (parametros.length == 3) {
+                    figura = new Triangulo(parametros[0], parametros[1], parametros[2]);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Número de parámetros incorrecto para un triángulo.");
+                    return;
+                }
+                break;
+            case "Circulo":
+                if (parametros.length == 1) {
+                    figura = new Circulo(parametros[0]);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Número de parámetros incorrecto para un círculo.");
+                    return;
+                }
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Figura no reconocida.");
+                return;
+        }
+        
+        if (figura != null) {
+            double perimetro = figura.calcPer();
+            double superficie = figura.calcSup();
+            vista.mostrarResultado(perimetro, superficie);
+            if (figura instanceof Triangulo) {
+                double altura = ((Triangulo) figura).calcularAltura();
+                vista.mostrarAltura(altura);
+            } else {
+                vista.ocultarAltura();
+            }
+    }
     
 }
