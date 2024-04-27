50a3dadeb44f3dcc0565706e22932d7ae1c04280
Nahuel <nahuel.ng240@gmail.com>
2024-04-26 23:34:50 -0300

    Separe los metodos de Triangulo con comentarios para que quede mejor identado

--- src/Model/Triangulo.java
+++ src/Model/Triangulo.java
@@ -5,24 +5,24 @@ import Interface.ICalcularFig;
 public class Triangulo implements ICalcularFig {
     private double lado1;
     private double lado2;
-    private double base;
+    private double lado3;
     private double altura;
 
-    public Triangulo(double lado1, double lado2, double base, double altura) {
+    public Triangulo(double lado1, double lado2, double lado3, double altura) {
         this.lado1 = lado1;
         this.lado2 = lado2;
-        this.base = base;
+        this.lado3 = lado3;
         this.altura = altura;
     }
 
-    public Triangulo(double lado1, double lado2, double base) {
+    public Triangulo(double lado1, double lado2, double lado3) {
         this.lado1 = lado1;
         this.lado2 = lado2;
-        this.base = base;
+        this.lado3 = lado3;
     }
 
     
-
+//<editor-fold defaultstate="collapsed" desc="Getters and Setters">
     public double getLado1() {
         return lado1;
     }
@@ -39,12 +39,12 @@ public class Triangulo implements ICalcularFig {
         this.lado2 = lado2;
     }
 
-    public double getBase() {
-        return base;
+    public double getlado3() {
+        return lado3;
     }
 
-    public void setBase(double base) {
-        this.base = base;
+    public void setlado3(double lado3) {
+        this.lado3 = lado3;
     }
 
     public double getAltura() {
@@ -54,21 +54,24 @@ public class Triangulo implements ICalcularFig {
     public void setAltura(double altura) {
         this.altura = altura;
     }
+//</editor-fold>
     
+//<editor-fold defaultstate="collapsed" desc="Metodos ICalcularFigura">
     @Override
-    public double calcSup() {
-        double sup = (lado1 + lado1 + base) / 2;
-        return (float) Math.sqrt(sup * (sup - lado1) * (sup - lado2) * (sup - base));
+    public double calcArea() {
+        double semiperimetro = calcPer() / 2;
+        return (double) Math.sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - lado3));
     }
 
     @Override
     public double calcPer() {
-        return lado1 + lado2 + base;
+        return lado1 + lado2 + lado3;
     }
     
      public double calcularAltura(){
-        double area = calcSup();
-        return (2 * area) / base;
+        double area = calcArea();
+        return (2 * area) / lado3;
         
     }
+//</editor-fold>
 }
